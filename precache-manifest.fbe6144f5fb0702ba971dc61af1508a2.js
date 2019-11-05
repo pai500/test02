b6a2895dc3fed82a36fb63bfb057e98e56d0eb48
pai500 <pai500@gmail.com>
2019-11-05 13:33:26 +0700

    Updates

---
+++ precache-manifest.fbe6144f5fb0702ba971dc61af1508a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9a354625cb38e65f1c65d9c2f9434e2",
+    "url": "/test02/index.html"
+  },
+  {
+    "revision": "c5dd1639467e8e77beac",
+    "url": "/test02/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "cad9a16b2108b238ad08",
+    "url": "/test02/static/js/2.17e293b0.chunk.js"
+  },
+  {
+    "revision": "c5dd1639467e8e77beac",
+    "url": "/test02/static/js/main.8455acc1.chunk.js"
+  },
+  {
+    "revision": "6a01c39d85c1a08da843",
+    "url": "/test02/static/js/runtime-main.1384d606.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/test02/static/media/logo.25bf045c.svg"
+  }
+]);
